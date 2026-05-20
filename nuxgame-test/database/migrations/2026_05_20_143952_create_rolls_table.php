25989f0c12946d1108e9e8b4ebb9f1af034095ba
 Den Sah <flyer2803@gmail.com>
2026-05-20 17:43:26 +0300

    nuxgame-test. migrations.

---
+++ nuxgame-test/database/migrations/2026_05_20_143952_create_rolls_table.php
@@ -0,0 +1,29 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('rolls', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullable(false)->constrained('users');
+            $table->smallInteger('number')->nullable(false)->unsigned();
+            $table->timestamp('created_at')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('rolls');
+    }
+};
